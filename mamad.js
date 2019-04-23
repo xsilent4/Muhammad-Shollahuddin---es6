d81bdf30342ebbe35a0be56a075a389a72b1b67f
xsilent4 <48704549+xsilent4@users.noreply.github.com>
2019-04-23 13:27:46 +0700

    Add files via upload

--- mamad.js
+++ mamad.js
@@ -1,11 +1,12 @@
 var Nama = 'Muhammad Shollahuddin H';
 alert(Nama);
 console.log(Nama);
-
 const MY_NUMBER = 10;
 alert(MY_NUMBER);
 console.log(MY_NUMBER);
-
 let b = 10;
+{
+  let b = 20; //deklarasi variabel dg let pada sebuah blok
 
   console.log(b); // output: 20
+}
